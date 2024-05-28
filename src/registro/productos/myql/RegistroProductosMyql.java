fa638621f313cc5fd7d9f1597826abc16fae5130
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-27 23:12:07 -0500

    llamada del formulario en la clase main

---
+++ src/registro/productos/myql/RegistroProductosMyql.java
@@ -0,0 +1,15 @@
+package registro.productos.myql;
+
+/**
+ *
+ * @author Jhonatan
+ * 
+ */
+public class RegistroProductosMyql {
+
+    public static void main(String[] args) {
+        frmProductos productos = new frmProductos();
+        productos.setVisible(true);
+    }
+    
+}
